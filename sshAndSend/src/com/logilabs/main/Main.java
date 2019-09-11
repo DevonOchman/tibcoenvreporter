7a216bfd191683ea3feafafd42e128c4e3cb0fc9
DevonOchman <devon.ochman@mail.mcgill.ca>
2019-09-11 16:21:58 -0400

    nn

--- sshAndSend/src/com/logilabs/main/Main.java
+++ sshAndSend/src/com/logilabs/main/Main.java
@@ -5,10 +5,14 @@ import com.logilabs.ssh.CommandExecutor;
 public class Main {
 
 	public static void main(String[] args) {
-		String user = "root";//"3033519";
-		String pwd = "h1TQy27DeE";//"3FbGPk28s";
-		String host = "192.168.43.147";//"lqc90990esbdvp01";
-		int port = 22;//2022;
+//		String user = "root";//"3033519";
+//		String pwd = "h1TQy27DeE";//"3FbGPk28s";
+//		String host = "192.168.43.147";//"lqc90990esbdvp01";
+//		int port = 22;//2022;
+		String user = "3033519";
+		String pwd = "3FbGPk28s";
+		String host = "lqc90990esbdvp01";
+		int port = 2022;
 
 		CommandExecutor cme = new CommandExecutor();
 		cme.addCommands("ps");
