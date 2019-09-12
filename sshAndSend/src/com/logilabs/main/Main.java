30ab4fa35039eb9072dea8e5cd5b3234a83bd96b
DevonOchman <devon.ochman@mail.mcgill.ca>
2019-09-12 15:08:44 -0400

    asdf

--- sshAndSend/src/com/logilabs/main/Main.java
+++ sshAndSend/src/com/logilabs/main/Main.java
@@ -35,17 +35,12 @@ public class Main {
 		}
 
 		String[] commands = { 
-//				"rm -f /home/root/newfile.txt",
-				"pwd",
-				"ls",
-				"sudo su - tibco", 
-				"pwd",
-				"ls",
+//				"sudo su - tibco", 
 				"wget ftp://toolsadm/tibco/scripts/audit/TibcoEnvReporter-0.0.1.jar",
-				"chmod +x TibcoEnvReporter-0.0.1.jar",
-				"java -jar TibcoEnvReporter-0.0.1.jar",
-				"echo y | mv report.txt /tmp/report.txt",
-				"chown " + user + " /tmp/report.txt",
+//				"chmod +x TibcoEnvReporter-0.0.1.jar",
+//				"java -jar TibcoEnvReporter-0.0.1.jar",
+//				"echo y | mv report.txt /tmp/report.txt",
+//				"chown " + user + " /tmp/report.txt",
 //				"rm -f TibcoEnvReporter-0.0.1.jar"
 				};
 
