25867c73af39ee472eaa445005a0114dbb274b0b
Devon Ochman <devon.ochman@mail.mcgill.ca>
2019-09-13 10:18:39 -0400

    Update tibco_install_report.sh

--- sshAndSend/tibco_install_report.sh
+++ sshAndSend/tibco_install_report.sh
@@ -4,5 +4,5 @@ wget ftp://toolsadm/tibco/scripts/audit/TibcoEnvReporter-0.0.1.jar
 chmod +x TibcoEnvReporter-0.0.1.jar
 java -jar TibcoEnvReporter-0.0.1.jar
 echo y | mv report.txt /tmp/report.txt
-chown  3033519  /tmp/report.txt
+chmod 777  /tmp/report.txt
 rm -f TibcoEnvReporter-0.0.1.jar
