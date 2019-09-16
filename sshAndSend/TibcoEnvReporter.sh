c3ea176da5c67398c9b10f6c0ed91c52b31dc227
DevonOchman <devon.ochman@mail.mcgill.ca>
2019-09-16 15:10:28 -0400

    wefghjhgre

--- sshAndSend/TibcoEnvReporter.sh
+++ sshAndSend/TibcoEnvReporter.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
-wget ftp://toolsadm/tibco/scripts/audit/TibcoEnvReporter-0.0.1.jar
-chmod +x TibcoEnvReporter-0.0.1.jar
-java -jar TibcoEnvReporter-0.0.1.jar
-echo y | mv report.txt /tmp/report.txt
-chmod 777  /tmp/report.txt
-rm -f TibcoEnvReporter-0.0.1.jar
+wget ftp://toolsadm/tibco/scripts/audit/TibcoEnvReporter-0.0.1.jar    
+chmod +x TibcoEnvReporter-0.0.1.jar    
+java -jar TibcoEnvReporter-0.0.1.jar    
+echo y | mv report.txt /tmp/report.txt     
+chmod 777  /tmp/report.txt    
+rm -f TibcoEnvReporter-0.0.1.jar    
